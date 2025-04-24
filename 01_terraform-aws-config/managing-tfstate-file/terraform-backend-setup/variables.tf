0c9c7f508627ab68bf64f5ef01087c8f3fd287e6
Ilsun Choi <ilsunchoi@cloudsecuritylab.co.kr>
2025-04-24 11:11:39 +0900

    프로파일 수정

--- 01_terraform-aws-config/managing-tfstate-file/terraform-backend-setup/variables.tf
+++ 01_terraform-aws-config/managing-tfstate-file/terraform-backend-setup/variables.tf
@@ -23,14 +23,14 @@ variable "environment" {
 
 # S3 버킷 이름 (Terraform 상태 파일 저장용)
 variable "s3_bucket_name" {
-  description = "Terraform 상태 파일을 저장할 S3 버킷 이름"     # S3 버킷 설명
-  type        = string                              # 문자열 타입
-  default     = "my-terraform-state-bucket-gasbugs" # 기본값: my-terraform-state-bucket-gasbugs
+  description = "Terraform 상태 파일을 저장할 S3 버킷 이름"      # S3 버킷 설명
+  type        = string                               # 문자열 타입
+  default     = "my-terraform-state-bucket-nickname" # 기본값: my-terraform-state-bucket-gasbugs
 }
 
 # DynamoDB 테이블 이름 (상태 잠금용)
 variable "dynamodb_table_name" {
   description = "Terraform 상태 잠금에 사용할 DynamoDB 테이블 이름" # DynamoDB 테이블 설명
   type        = string                                 # 문자열 타입
-  default     = "terraform-state-lock-gasbugs"         # 기본값: terraform-state-lock-gasbugs
+  default     = "terraform-state-lock-nickname"        # 기본값: terraform-state-lock-gasbugs
 }
